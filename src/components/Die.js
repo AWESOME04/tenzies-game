eba3a51c9725d5e8213567e72e18b01cb37c262d
Evans Acheampong <evansachie01@gmail.com>
2024-04-26 23:02:31 +0000

    added project files

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,12 +1,13 @@
 import React from "react"
+import "../index.css"
 
 export default function Die(props) {
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
     return (
-        <div 
-            className="die-face" 
+        <div
+            className="die-face"
             style={styles}
             onClick={props.holdDice}
         >
